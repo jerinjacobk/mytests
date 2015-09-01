819e04d42d345e5a2cc1152703c42ca8f1023807
Jerin Jacob <jerin.jacob@caviumnetworks.com>
2015-09-01 09:03:50 +0530

    new _mm functions

--- sse_to_neon/test.c
+++ sse_to_neon/test.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 #define INLINE inline __attribute__((always_inline))
-#define NOINLINE inline __attribute__((no_inline))
+#define NOINLINE inline __attribute__((noinline))
 
 #ifdef __aarch64__
 
@@ -20,6 +20,38 @@ static INLINE __m128i _mm_set_epi64x(int64_t i1, int64_t i0)
                 "ins %[out].D[1], %x[i1]" "\n\t"
                 :[out]"=w"(out)
                 :[i1]"r"(i1),[i0]"r"(i0)
+                );
+        return out;
+}
+
+static INLINE __m128i _mm_setzero_si128()
+{
+#if 0
+        __m128i out;
+        asm volatile(
+                "movi %[out].4S, #0" "\n\t"
+                :[out]"=w"(out)
+                );
+        return out;
+#else
+	return vdupq_n_s32(0);
+#endif
+}
+
+static INLINE void _mm_store_si128(__m128i *p, __m128i a)
+{
+	vst1q_s32((int32_t*) p,a);
+}
+
+
+static INLINE __m128i _mm_unpackhi_epi64 (__m128i a, __m128i b)
+{
+        __m128i out;
+        asm volatile(
+                "mov %[out].D[0], %[a].D[1]" "\n\t"
+                "mov %[out].D[1], %[b].D[1]" "\n\t"
+                :[out]"=&w"(out)
+                :[a]"w"(a),[b]"w"(b)
                 );
         return out;
 }
@@ -72,8 +104,20 @@ void print128_num(__m128i var)
 int main()
 {
 	__m128i var;
+	__m128i a;
+	__m128i b;
 	var = _mm_set_epi64x(0xdeadbeef, 0x12345678);
 	print128_num(var);
+	var = _mm_setzero_si128();
+	print128_num(var);
+
+	a = _mm_set_epi64x(0xdeadbeef, 0x12345678);
+	print128_num(a);
+	b = _mm_set_epi64x(0x9abcdef, 0xaaaaabbbb);
+	print128_num(b);
+	var = _mm_unpackhi_epi64(a, b);
+	print128_num(var);
+
 	return 0;
 }
 
