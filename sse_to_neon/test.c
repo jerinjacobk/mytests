00b48886be6b4ae6dd679f3eb863b7f26aa2f341
Jerin Jacob <jerin.jacob@caviumnetworks.com>
2016-06-21 17:39:38 +0530

    virtio

--- sse_to_neon/test.c
+++ sse_to_neon/test.c
@@ -100,7 +100,7 @@ void print128_u16_num(__m128i var)
 void print128_u8_num(__m128i var)
 {
 	uint8_t *val = (uint8_t*) &var;
-	printf("u8: %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i\n",
+	printf("u8: %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n",
            val[15], val[14], val[13], val[12], val[11], val[10],
            val[9], val[8], val[7], val[6], val[5], val[4],
 	   val[3], val[2], val[1], val[0]);
@@ -153,14 +153,69 @@ void test0(void)
 #endif
 }
 
+uint64_t ptr_array[64];
+
+void init(void)
+{
+	ptr_array[0] = 0x0001020304050607;
+	ptr_array[1] = 0x08090a0b0c0d0e0f;
+
+}
+
+
 void test1(void)
 {
-	__m128i var;
-	__m128i mask;
-	__m128i res;
+	uint64_t *rused;
 
-	print128_num(res);
+	__m128i desc;
+	__m128i pkt_mb[2];
+	__m128i shuf_msk1, shuf_msk2, len_adjust;
 
+	init();
+	rused = &ptr_array[0];
+
+	shuf_msk1 = _mm_set_epi8(
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF,		/* vlan tci */
+		5, 4,			/* dat len */
+		0xFF, 0xFF, 5, 4,	/* pkt len */
+		0xFF, 0xFF, 0xFF, 0xFF	/* packet type */
+
+	);
+
+	shuf_msk2 = _mm_set_epi8(
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF,		/* vlan tci */
+		13, 12,			/* dat len */
+		0xFF, 0xFF, 13, 12,	/* pkt len */
+		0xFF, 0xFF, 0xFF, 0xFF	/* packet type */
+	);
+
+	/* Subtract the header length.
+	*  In which case do we need the header length in used->len ?
+	*/
+	len_adjust = _mm_set_epi16(
+		0, 0,
+		0,
+		(uint16_t)-1,
+		0, (uint16_t)-1,
+		0, 0);
+
+
+	desc = _mm_loadu_si128((__m128i *)(rused + 0));
+
+	pkt_mb[1] = _mm_shuffle_epi8(desc, shuf_msk2);
+	pkt_mb[0] = _mm_shuffle_epi8(desc, shuf_msk1);
+	pkt_mb[1] = _mm_add_epi16(pkt_mb[1], len_adjust);
+	pkt_mb[0] = _mm_add_epi16(pkt_mb[0], len_adjust);
+
+
+	print128_u8_num(desc);
+	print128_u8_num(shuf_msk1);
+	print128_u8_num(shuf_msk2);
+	print128_u8_num(len_adjust);
+	print128_u8_num(pkt_mb[0]);
+	print128_u8_num(pkt_mb[1]);
 }
 
 int main()
